ceb7087a61d32a376e13a75a0d381ad8bf7e6a96
DannaVilllallba <dannavillallba541@gmail.com>
2024-08-14 10:48:09 -0600

    dana

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -11,60 +11,70 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/camion2.png')}
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <View style={styles.container}>
+      <Text style={{ color: '#ffffff', fontSize: 40 }}>Login</Text>
+      <Text style={{ color: '#ffffff', fontSize: 20, top: 7}}> Please sing in to continue</Text>
+      </View>
+      <TextInput style = {styles.caja1}>
+        <Text>
+          <Text style={{ fontWeight: 900, fontSize: 20,color: '#ffffff' }}>Ingrese Gmail</Text>
+        </Text>
+      </TextInput>
+      <TextInput style = {styles.caja2}>
+        <Text>
+          <Text style={{ fontWeight: 900, fontSize: 20,color: '#ffffff' }}>Ingrese Contraseña</Text>
+        </Text>
+      </TextInput>
+      <TouchableOpacity style ={styles.login}>
+        <Text style={{fontSize: 20,color: '#ffffff',textAlign:'center', marginTop: 19, }}>Ingresar</Text>
+      </TouchableOpacity>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 250,
+    width: 430,
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  caja1: {
+    backgroundColor: "#3c3c3c",
+    height: 60, 
+    width: 340,
+    borderRadius: 30,
+    marginTop: 40 ,
+    textAlign:'center',
+  },
+  caja2: {
+    backgroundColor: "#3c3c3c",
+    height: 60, 
+    width: 340,
+    borderRadius: 30,
+    marginTop: 30 ,
+    textAlign:'center',
+  },
+  login: {
+    backgroundColor: "#3c3c3c",
+    height: 60, 
+    width: 150,
+    borderRadius: 30,
+    marginTop: 30 ,
+    marginLeft: 100,  
   },
 });
